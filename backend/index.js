db0a3b5bde63866b0a9320c5b5ba52a3e3c6443b
Tyler Tran <trannhattien28032002@gmail.com>
2025-02-26 19:58:37 +0700

    Database Setup

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./db/connectDB.js";
+
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
+    connectDB();
     console.log("Server is running on port 3000")
 })
