0ead8708e7b7d327a4c0149d721c3bc3554bfe6f
Tyler Tran <trannhattien28032002@gmail.com>
2025-03-02 23:50:19 +0700

    Verify Email Endpoint

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -19,3 +19,5 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         throw new Error(`Error sending verification email: ${error}`);
     }
 }
+
+export const sendWelcomeEmail = async (email, name) => {}
