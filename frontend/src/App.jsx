4a0563b681d4e4f1da6fa29ed8ee4b0b4f9c7110
Tyler Tran <trannhattien28032002@gmail.com>
2025-03-04 23:58:13 +0700

    Email Validation Page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,6 +32,7 @@ function App() {
         <Route path="/" element={"Home"} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/verify-email" element={<EmailVerificationPage />} />
       </Routes>
     </div>
   );
